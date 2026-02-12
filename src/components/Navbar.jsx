d39a3bfa6f8c756e1fec01782e85b88943a23f42
200-hassan <md.arhassan@gmail.com>
2026-02-13 04:38:03 +0600

    nav bottom part

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,63 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { PhoneCall } from "lucide-react";
+import { ChevronDown } from "lucide-react";
 const Navbar = () => {
   return (
     <div>
-      <ul className="flex items-center justify-center gap-10 pt-10">
-        <Link to="/">
-          <li>Home</li>
-        </Link>
-        <Link to="/shop">
-          <li>Shop</li>
-        </Link>
-        <Link to="/pages">
-          <li>Pages</li>
-        </Link>
-        <Link to="/blog">
-          <li>Blog</li>
-        </Link>
-        <Link to="/about">
-          <li>About Us</li>
-        </Link>
-        <Link to="/contact">
-          <li>Contact Us</li>
-        </Link>
-      </ul>
+      {/* search bar */}
+      <div className="bg-white border-10"></div>
+      <div className="flex items-center justify-between container mx-auto">
+        {/* Nav Items */}
+        <div>
+          <ul className="flex items-center justify-center gap-10 pt-10 text-white pb-5">
+            <Link to="/">
+              <li className="flex gap-1">
+                Home
+                <span>
+                  <ChevronDown />
+                </span>
+              </li>
+            </Link>
+            <Link to="/shop">
+              <li className="flex gap-1">
+                Shop
+                <span>
+                  <ChevronDown />
+                </span>
+              </li>
+            </Link>
+            <Link to="/pages">
+              <li className="flex gap-1">
+                Pages
+                <span>
+                  <ChevronDown />
+                </span>
+              </li>
+            </Link>
+            <Link to="/blog">
+              <li className="flex gap-1">
+                Blog
+                <span>
+                  <ChevronDown />
+                </span>
+              </li>
+            </Link>
+            <Link to="/about">
+              <li>About Us</li>
+            </Link>
+            <Link to="/contact">
+              <li>Contact Us</li>
+            </Link>
+          </ul>
+        </div>
+
+        {/* Phone Number */}
+        <div className="flex pt-10 text-white gap-2">
+          <PhoneCall />
+          <p>(219) 555-0114</p>
+        </div>
+      </div>
     </div>
   );
 };
