47b69f1cae069f8a8451e53716201e2608c090d7
200-hassan <md.arhassan@gmail.com>
2026-02-13 19:44:01 +0600

    Search bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,52 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { PhoneCall } from "lucide-react";
 import { ChevronDown } from "lucide-react";
+import Plant1 from "../assets/plant 1.jpg";
+import { Search, Heart, Tally1, Handbag } from "lucide-react";
+
 const Navbar = () => {
   return (
     <div>
       {/* search bar */}
-      <div className="bg-white border-10"></div>
+      <div className="bg-white">
+        <div className=" flex items-center justify-between mx-auto container">
+          {/* left side */}
+          <div className="flex gap-3 pt-7 pb-7 items-center">
+            <img src={Plant1} alt="" className="h-8 w-8" />
+            <p className="text-green-900 text-3xl font-sm">Ecobazar</p>
+          </div>
+
+          {/* search box */}
+          <div className="flex items-center">
+            <div className="border-1 border-r-0 border-gray-400 rounded-l-lg pt-6 pb-6 mt-6 mb-6 h-12 w-125 flex">
+              <div className="flex items-center">
+                <Search className="text-black ml-4" />
+                <text className="text-gray-500 ml-2">Search</text>
+              </div>
+            </div>
+            <div>
+              <button className="text-white bg-green-500 rounded-r-lg h-13 w-25">
+                Search
+              </button>
+            </div>
+          </div>
+
+          {/* right sight */}
+          <div className="flex items-center">
+            <div className="flex items-center">
+              <Heart className="text-black" />
+              <Tally1 className="text-gray-500 ml-3" />
+              <Handbag className="text-black" />
+            </div>
+            <div>
+              <p className="text-gray-500 ml-3">Shopping cart:</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Nav Items */}
       <div className="flex items-center justify-between container mx-auto">
-        {/* Nav Items */}
         <div>
           <ul className="flex items-center justify-center gap-10 pt-10 text-white pb-5">
             <Link to="/">
